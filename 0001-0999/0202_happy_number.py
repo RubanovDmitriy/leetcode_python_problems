5b101d7d38af13a7e03b0962d1d512e7fed0a9b5
dmitriirubanov <dmitriyrubanov1988@gmail.com>
2024-06-15 21:43:12 +0400

    202. Happy Number

---
+++ 0001-0999/0202_happy_number.py
@@ -0,0 +1,9 @@
+def is_happy(n: int) -> bool:
+    while n != 1 and n != 4:
+        _sum = 0
+        while n:
+            _sum += (n % 10) ** 2
+            n //= 10
+        n = _sum
+
+    return n == 1
