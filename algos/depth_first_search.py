aee4abb163f6422df4727bdb34e40ac40d832f32
dmitriirubanov <dmitriyrubanov1988@gmail.com>
2024-06-15 16:10:38 +0400

    depth first search

---
+++ algos/depth_first_search.py
@@ -0,0 +1,19 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+
+    graph_start = graph[start]
+
+    for next in graph[start] - visited:
+        dfs(graph, next, visited)
+    return visited
+
+
+graph = {'0': {'1', '2'},
+         '1': {'0', '3', '4'},
+         '2': {'0'},
+         '3': {'1'},
+         '4': {'2', '3'}}
+
+dfs(graph, '0')
